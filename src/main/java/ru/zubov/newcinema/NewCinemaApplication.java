023a38403a1ca70eecbd44bfc18c857326cfead0
Vitaly Zubov <Zubov.VP@yandex.ru>
2024-01-03 23:46:03 +0300

    Initial commit

---
+++ src/main/java/ru/zubov/newcinema/NewCinemaApplication.java
@@ -0,0 +1,13 @@
+package ru.zubov.newcinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewCinemaApplication.class, args);
+	}
+
+}
