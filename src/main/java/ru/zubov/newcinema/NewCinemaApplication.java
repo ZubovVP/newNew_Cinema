80f5caa0db9cfc865be3aaeee8744934b0e23b72
Vitaly Zubov <Zubov.VP@yandex.ru>
2024-01-08 23:36:56 +0300

    Correct rung Liquibase and create tables

--- src/main/java/ru/zubov/newcinema/NewCinemaApplication.java
+++ src/main/java/ru/zubov/newcinema/NewCinemaApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NewCinemaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NewCinemaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(NewCinemaApplication.class, args);
+    }
 }
